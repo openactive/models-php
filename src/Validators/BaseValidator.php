3739cabf589f4f4136f4ae802e39caf5934e3075
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-14 13:36:40 +0100

    Added initial Validator implementation

---
+++ src/Validators/BaseValidator.php
@@ -0,0 +1,54 @@
+<?php
+
+class BaseValidator implements ValidatorInterface
+{
+    /**
+     * Coerce given value to the type the validator is validating against.
+     *
+     * @param mixed $value The value to coerce.
+     * @return mixed The same value.
+     */
+    public function coerce($value)
+    {
+        // For all the classes that inherit BaseValidator
+        // that don't need to coerce,
+        // This is a no-op
+        return $value;
+    }
+
+    /**
+     * Run validation on the given value.
+     *
+     * @param mixed $value The value to validate.
+     * @return bool Whether validation passes or not.
+     */
+    public function run($value)
+    {
+        return false;
+    }
+
+    /**
+     * Returns a validator instance from a given type name.
+     *
+     * @param string The type name
+     * @return \OpenActive\Validators\ValidatorInterface
+     */
+    public static function getValidator($type)
+    {
+        // If first letter of type is a lower case letter
+        if(ctype_lower(substr($type, 0, 1)) === true) {
+            // We are validating a native type
+            $validatorName = ucfirst($type)."Validator";
+
+            return new $validatorName();
+        }
+
+        if($type === "DateTime" || $type === "DateInterval") {
+            $classname = "\\".$type;
+        } else {
+            $classname = "\\OpenActive\\Models\\".$type;
+        }
+
+        return new InstanceValidator($classname);
+    }
+}
---
+++ src/Validators/ValidatorInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+interface ValidatorInterface
+{
+    /**
+     * Coerce given value to the type the validator is validating against.
+     *
+     * @param mixed $value The value to coerce.
+     * @return mixed
+     */
+    public function coerce($value);
+
+    /**
+     * Run validation on the given value.
+     *
+     * @param mixed $value The value to validate.
+     * @return bool Whether validation passes or not.
+     */
+    public function run($value);
+}
