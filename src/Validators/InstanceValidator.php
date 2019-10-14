bfa220bea593e6d90321c29796b56f0c7a0e3667
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-14 13:39:04 +0100

    Added instance validator

---
+++ src/Validators/InstanceValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+class InstanceValidator implements ValidatorInterface
+{
+    public function __construct($classname)
+    {
+        $this->classname = $classname;
+    }
+
+    /**
+     * Run validation on the given value.
+     *
+     * @param mixed $value The value to validate.
+     * @return bool Whether validation passes or not.
+     */
+    public function run($value)
+    {
+        // instanceof does not throw any error
+        // if the variable being tested is not an object,
+        // it simply returns FALSE.
+        return $value instanceof $this->classname;
+    }
+}
