364203b042babec0ae8ed0aefdd52df4b75c3da9
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-14 13:38:37 +0100

    Added native type validators

---
+++ src/Validators/StringValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+class StringValidator extends BaseValidator
+{
+    /**
+     * Run validation on the given value.
+     *
+     * @param mixed $value The value to validate.
+     * @return bool Whether validation passes or not.
+     */
+    public function run($value)
+    {
+        return is_string($value);
+    }
+}
