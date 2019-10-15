8cd8feb132877c0b72897977d172b959c8bea64b
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-15 16:43:54 +0100

    Added DateTime Helper

---
+++ src/Helpers/DateTime.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OpenActive\Helpers;
+
+/**
+ * A set of helper methods around the global PHP DateTime class.
+ */
+class DateTime
+{
+    /**
+     * Returns a ISO 8601 formatted date string from a given DateTime object.
+     *
+     * @param \DateTime $date
+     * @return string
+     * @see https://www.php.net/manual/en/function.date.php#refsect1-function.date-parameters
+     */
+    public static function iso8601($date)
+    {
+        if($dateInterval instanceof \DateTime === FALSE) {
+            throw new \Exception("Invalid argument type.");
+        }
+
+        return $date->format("c");
+    }
+}
