c9908a5cfc0c9cf737c5298b9fd241a747cc706c
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-15 16:42:54 +0100

    DateInterval helper re-factoring

--- src/Helpers/DateInterval.php
+++ src/Helpers/DateInterval.php
@@ -3,7 +3,7 @@
 namespace OpenActive\Helpers;
 
 /**
- *
+ * A set of helper methods around the global PHP DateInterval class.
  */
 class DateInterval
 {
@@ -11,8 +11,9 @@ class DateInterval
      * Returns an interval spec string (ISO 8601 format)
      * from a given DateInterval object.
      *
-     * @param DateInterval $dateInterval
+     * @param \DateInterval $dateInterval
      * @return string
+     * @see https://stackoverflow.com/a/25371691
      */
     public static function specString($dateInterval)
     {
@@ -22,26 +23,26 @@ class DateInterval
 
         // Reading all non-zero date parts.
         $date = array_filter(array(
-            'Y' => $dateInterval->y,
-            'M' => $dateInterval->m,
-            'D' => $dateInterval->d
+            "Y" => $dateInterval->y,
+            "M" => $dateInterval->m,
+            "D" => $dateInterval->d
         ));
 
         // Reading all non-zero time parts.
         $time = array_filter(array(
-            'H' => $dateInterval->h,
-            'M' => $dateInterval->i,
-            'S' => $dateInterval->s
+            "H" => $dateInterval->h,
+            "M" => $dateInterval->i,
+            "S" => $dateInterval->s
         ));
 
-        $specString = 'P';
+        $specString = "P";
 
         // Adding each part to the spec-string.
         foreach ($date as $key => $value) {
             $specString .= $value . $key;
         }
         if (count($time) > 0) {
-            $specString .= 'T';
+            $specString .= "T";
             foreach ($time as $key => $value) {
                 $specString .= $value . $key;
             }
