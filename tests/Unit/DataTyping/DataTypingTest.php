c1e166e147780b881b10de03c637c42264ae0fb5
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-17 16:26:23 +0100

    Added test type is present and populated and correct

--- tests/Unit/DataTyping/DataTypingTest.php
+++ tests/Unit/DataTyping/DataTypingTest.php
@@ -20,6 +20,54 @@ use PHPUnit\Framework\TestCase;
 class DataTypingTest extends TestCase
 {
     /**
+     * Test that type is present and populated correctly.
+     *
+     * @return void
+     */
+    public function testTypeIsPresentAndCorrect()
+    {
+        $testData = array();
+
+        $classname = "\\OpenActive\\Models\\OA\\Event";
+
+        $testData[0] = array(
+            file_get_contents(__DIR__ . "/../event.json"),
+            $classname
+        );
+
+        $classname = "\\OpenActive\\Models\\OA\\FacilityUse";
+
+        $testData[1] = array(
+            file_get_contents(__DIR__ . "/../facility_use.json"),
+            $classname
+        );
+
+        $classname = "\\OpenActive\\Models\\OA\\Order";
+
+        $testData[2] = array(
+            file_get_contents(__DIR__ . "/../order.json"),
+            $classname
+        );
+
+        foreach($testData as $testItem) {
+            // Get purely decoded JSON
+            $decodedJson = json_decode($testItem[0], true);
+
+            // Get the actual deserialized then serialized, JSON-decoded data
+            $serializedData = json_decode(
+                $testItem[1]::serialize(
+                    $testItem[1]::deserialize($testItem[0])
+                ),
+                true
+            );
+
+            $this->assertTrue(array_key_exists("type", $serializedData));
+            $this->assertTrue(empty($serializedData["type"]) === FALSE);
+            $this->assertEquals($decodedJson["type"], $serializedData["type"]);
+        }
+    }
+
+    /**
      * Test that null values are absent.
      *
      * @dataProvider jsonProvider
