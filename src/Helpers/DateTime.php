bc68898ba99683ceb5359ea9c6fd48d7358195d4
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-18 11:45:39 +0100

    DateTime helper now uses only date format if requested

--- src/Helpers/DateTime.php
+++ src/Helpers/DateTime.php
@@ -8,18 +8,29 @@ namespace OpenActive\Helpers;
 class DateTime
 {
     /**
-     * Returns a ISO 8601 formatted date string from a given DateTime object.
+     * Returns a ISO 8601 formatted date-time string from a given DateTime object.
+     * If the isDateOnly parameter is specified, return the ISO 8601 date string.
      *
      * @param \DateTime $date
+     * @param bool|null $isDateOnly
      * @return string
      * @see https://www.php.net/manual/en/function.date.php#refsect1-function.date-parameters
      */
-    public static function iso8601($date)
+    public static function iso8601($date, $isDateOnly = false)
     {
         if($date instanceof \DateTime === FALSE) {
             throw new \Exception("Invalid argument type.");
         }
 
-        return $date->format("c");
+        // If only the date is needed, return ISO 8601 date format
+        if($isDateOnly === true) {
+            $format = "Y-m-d";
+
+        // Otherwise perform the usual format e.g. 2019-01-01T12:00:00-08:00
+        } else {
+            $format = "Y-m-d\TH:i:sP";
+        }
+
+        return $date->format($format);
     }
 }
