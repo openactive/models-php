2e7c1dade0667b89adf97fcf199a8d79f18116b8
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-21 16:37:34 +0100

    RpdeBody must be created from a static method (constructor is private)

--- src/Rpde/RpdeBody.php
+++ src/Rpde/RpdeBody.php
@@ -39,7 +39,7 @@ class RpdeBody implements SerializerInterface, TypeCheckerInterface
      * @param array $data
      * @return void
      */
-    public function __construct($data)
+    private function __construct($data)
     {
         foreach ($data as $key => $value) {
             // Make sure setter is cased properly
@@ -49,8 +49,116 @@ class RpdeBody implements SerializerInterface, TypeCheckerInterface
                 $this->$methodName($value);
             }
         }
+    }
 
+    /**
+     * @param string $feedBaseUrl
+     * @param string $modified
+     * @param string $id
+     * @param array $items
+     * @return self
+     * @throws \Exception If on or more of the provided arguments are not of a supported type.
+     */
+    public static function createFromModifiedId(
+        $feedBaseUrl,
+        $modified,
+        $id,
+        $items
+    )
+    {
+        // If the items provided are not valid,
+        // An exception will be thrown,
+        // We don't implement any further type checks on $items after this
+        $rpdeBody = new self([
+            "items" => $items,
+            "license" => static::$default_license,
+        ]);
 
+        // If there is at least one item, run validation on items array
+        if (count($items) > 0) {
+            $firstItem = $items[0];
+
+            // Checks that the afterId and afterTimestamp provided are not the
+            // first item in the feed (helps detect whether query is correct)
+            if (
+                $firstItem->getModified() === $modified &&
+                $firstItem->getId() === $id
+            ) {
+                throw new \Exception(
+                    "First item in the feed must never have same 'modified' ".
+                    "and 'id' as afterTimestamp and afterId query parameters. ".
+                    "Please check the RPDE specification and ensure you are ".
+                    "using the correct query for your ordering strategy."
+                );
+            }
+
+            // Check that items are ordered, and deleted items contain no data
+            $currentModified = -1;
+            $currentId = $firstItem->getId();
+            foreach ($items as $item) {
+                $itemState = $item->getState();
+                $itemModified = $item->getModified();
+                $itemId = $item->getId();
+                if (
+                    $itemState === RpdeState::DELETED &&
+                    $item->getData() !== null
+                ) {
+                    throw new \Exception("Deleted items must not contain data.");
+                }
+
+                if (
+                    $itemState === null ||
+                    $item->getKind() === null ||
+                    $itemModified === null ||
+                    $itemId == null
+                ) {
+                    throw new \Exception(
+                        "All RPDE feed items must include id, modified, state ".
+                        "and kind."
+                    );
+                }
+
+                if (
+                    $itemModified > $currentModified ||
+                    (
+                        $itemModified === $currentModified &&
+                        $itemId > $currentId
+                    )
+                ) {
+                    $currentModified = $itemModified;
+                    $currentId = $itemId;
+                } else {
+                    throw new \Exception(
+                        "Items must be ordered first by 'modified', ".
+                        "then by 'id'. Please check the RPDE specification ".
+                        "and ensure you are using the correct query for ".
+                        "your ordering strategy."
+                    );
+                }
+            }
+
+            // Create 'next' URL depending on whether there are items available
+            $lastItemIdx = count($items) - 1;
+            $lastItem = $items[$lastItemIdx];
+            $rpdeBody->setNext(
+                $feedBaseUrl."?afterTimestamp=".$lastItem->getModified().
+                "&afterId=".$lastItem->getId()
+            );
+        } else {
+            // Set "next" value
+
+            // Last page, use existing values
+            if ($modified !== null && $id !== null) {
+                $rpdeBody->setNext(
+                    $feedBaseUrl."?afterTimestamp=".$modified."&afterId=".$id
+                );
+            } else {
+                // No items, use feed base URL
+                $rpdeBody->setNext($feedBaseUrl);
+            }
+        }
+
+        return $rpdeBody;
     }
     /**
      * @return string
