aa0d836ec1fecba93c12b2b451e49cf50ca003cf
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-23 11:31:26 +0100

    InvalidArgumentException implements OpenActiveExceptionInterface

--- src/Exceptions/InvalidArgumentExcpetion.php
+++ src/Exceptions/InvalidArgumentExcpetion.php
@@ -2,9 +2,10 @@
 
 namespace OpenActive\Exceptions;
 
+use OpenActive\Contracts\OpenActiveExceptionInterface;
 use InvalidArgumentException as BaseInvalidArgumentException;
 
-class InvalidArgumentException extends BaseInvalidArgumentException
+class InvalidArgumentException extends BaseInvalidArgumentException implements OpenActiveExceptionInterface
 {
     //
 }
