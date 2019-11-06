4abf1f01507db4f6451ad518fb1ea3e8ca892246
Henry Addison <henryaddison@users.noreply.github.com>
2019-11-06 15:53:41 +0000

    add a TimeValidator
    with tests

---
+++ src/Validators/TimeValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OpenActive\Validators;
+
+class TimeValidator extends BaseValidator
+{
+    /**
+     * Run validation on the given value.
+     *
+     * @param mixed $value The value to validate.
+     * @return bool Whether validation passes or not.
+     */
+    public function run($value)
+    {
+        if(!is_string($value))
+            return false;
+        // user DateTime to parse the string to check it it a valid time string
+        // alas it will add today's date so this created object cannot
+        $dateTimeFormat = "H:i:s";
+        $time = \DateTime::createFromFormat($dateTimeFormat, $value);
+
+        return $time !== false;
+    }
+}
