70900f250ca534f02d967fea00f0b8f5651af96d
Joe Carter <theupperquartile@gmail.com>
2020-05-05 11:20:17 +0100

    Make script more consistent between Ruby+PHP

--- update-models.sh
+++ update-models.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-MODELS_LIB_DIR="${BASE_DIR}/vendor/models-lib"
 
+MODELS_LIB_DIR="${BASE_DIR}/vendor/models-lib";
+TARGET_DIR="${BASE_DIR}/src";
+GENERATED_DIRS=("${TARGET_DIR}/Models" "${TARGET_DIR}/Enums");
+
+mkdir -p "$(dirname "${MODELS_LIB_DIR}")";
 if [[ ! -d "${MODELS_LIB_DIR}" ]]; then
-  git clone https://github.com/openactive/models-lib "${MODELS_LIB_DIR}"
+  git clone https://github.com/openactive/models-lib "${MODELS_LIB_DIR}";
 fi
 
 (
-  cd "${MODELS_LIB_DIR}"
-  command -v nvm && nvm use
+  cd "${MODELS_LIB_DIR}";
+  command -v nvm && nvm use;
 
   if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
-    git pull
-    npm install
+    git pull;
+    npm install;
   fi
-  rm -rf "${BASE_DIR}/src/Models" "${BASE_DIR}/src/Enums"
-  npm run app-dev -- generate PHP --destination "${BASE_DIR}/src"
+  rm -rf "${GENERATED_DIRS[@]}";
+  npm run app-dev -- generate PHP --destination "${TARGET_DIR}";
 )
 
-COMPOSER_BIN=$(command -v composer || command -v composer.phar || command -v ./composer.phar)
+COMPOSER_BIN=$(command -v composer || command -v composer.phar || command -v ./composer.phar);
 
-"${COMPOSER_BIN}" fix "${BASE_DIR}/src/models"
-"${COMPOSER_BIN}" fix "${BASE_DIR}/src/enums"
+for dir in ${GENERATED_DIRS[@]}; do
+  "${COMPOSER_BIN}" fix "${dir}";
+done
+"${COMPOSER_BIN}" test;
 
-"${COMPOSER_BIN}" test
-
-git add "${BASE_DIR}/src/models" "${BASE_DIR}/src/enums"
+git add "${GENERATED_DIRS}";
