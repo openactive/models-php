7cf7087ef0940f69bca015ae75f4c673830baca1
Joe Carter <theupperquartile@gmail.com>
2020-05-05 10:10:29 +0100

    Address PR feedback

--- update-models.sh
+++ update-models.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-export BASE_DIR=$(pwd);
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+MODELS_LIB_DIR="${BASE_DIR}/vendor/models-lib"
 
-if [[ ! -d models-lib ]]; then
-  git clone https://github.com/openactive/models-lib "${BASE_DIR}/vendor/models-lib"
+if [[ ! -d "${MODELS_LIB_DIR}" ]]; then
+  git clone https://github.com/openactive/models-lib "${MODELS_LIB_DIR}"
 fi
 
 (
-  cd models-lib
+  cd "${MODELS_LIB_DIR}"
+  command -v nvm && nvm use
 
   if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
     git pull
@@ -17,7 +19,7 @@ fi
   npm run app-dev -- generate PHP --destination "${BASE_DIR}/src"
 )
 
-export COMPOSER_BIN=$(command -v composer || command -v composer.phar || command -v ./composer.phar)
+COMPOSER_BIN=$(command -v composer || command -v composer.phar || command -v ./composer.phar)
 
 "${COMPOSER_BIN}" fix "${BASE_DIR}/src/models"
 "${COMPOSER_BIN}" fix "${BASE_DIR}/src/enums"
