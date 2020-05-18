2a749431621b57e3dff349d660f1d75e510d6b1b
Joe Carter <theupperquartile@gmail.com>
2020-05-18 13:53:15 +0100

    Add node version checking

--- update-models.sh
+++ update-models.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Colour
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
@@ -14,6 +19,9 @@ fi
 (
   cd "${MODELS_LIB_DIR}";
   command -v nvm && nvm use;
+  npx check-node-version --node $(cat .nvmrc) || (
+    >&2 echo -e "${YELLOW}OpenActive: ${RED}Please install and activate Node $(cat .nvmrc), ${YELLOW}nvm${RED} is recommended.${NC}" && exit 1
+  );
 
   if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
     git pull;
