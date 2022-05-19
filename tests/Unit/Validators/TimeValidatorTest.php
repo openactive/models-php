a2236d9f7aa80b2792f308b3d97520a8627c26c3
Nathan Salter <elvenlie@gmail.com>
2022-05-19 12:10:51 +0100

    Fixes #98 - Fix time parsing with seconds (#99)
    Co-authored-by: Nathan Salter <nathan@playfinder.com>

--- tests/Unit/Validators/TimeValidatorTest.php
+++ tests/Unit/Validators/TimeValidatorTest.php
@@ -10,6 +10,7 @@ class TimeValidatorTest extends TestCase
     public function testValidTimeReturnsTrue()
     {
         $this->assertTrue((new TimeValidator())->run("12:00:00"), "TimeValidator should allow valid ISO 8601 times");
+        $this->assertTrue((new TimeValidator())->run("12:00"), "TimeValidator should allow valid ISO 8601 times");
     }
 
     public function testInvalidTimeReturnsFalse()
